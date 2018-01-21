8baf849d21523e91bfe97dd0fd61f652e3bd4445
Dani <kemenydani93@gmail.com>
2018-01-21 19:55:12 +0100

    progress

--- src/components/views/gateway/index.vue
+++ src/components/views/gateway/index.vue
@@ -1,15 +1,88 @@
 <template>
-	<div>
-		gateway
-	</div>
+	<v-container>
+		<v-layout row v-if="error">
+			<v-flex xs12 sm6 offset-sm3>
+				<app-alert @dismissed="onDismissed" :text="error.message"></app-alert>
+			</v-flex>
+		</v-layout>
+		<v-layout row>
+			<v-flex xs12 sm6 offset-sm3>
+				<v-card color="blue">
+					<v-card-text>
+						<v-container>
+							<form @submit.prevent="onSignin">
+								<v-layout row>
+									<v-flex xs12>
+										<v-text-field
+												name="email"
+												label="Mail"
+												id="email"
+												v-model="email"
+												type="email"
+												required></v-text-field>
+									</v-flex>
+								</v-layout>
+								<v-layout row>
+									<v-flex xs12>
+										<v-text-field
+												name="password"
+												label="Password"
+												id="password"
+												v-model="password"
+												type="password"
+												required></v-text-field>
+									</v-flex>
+								</v-layout>
+								<v-layout row>
+									<v-flex xs12>
+										<v-btn type="submit" :disabled="loading" :loading="loading">
+											Sign in
+											<span slot="loader" class="custom-loader">
+                        <v-icon light>cached</v-icon>
+                       </span>
+										</v-btn>
+									</v-flex>
+								</v-layout>
+							</form>
+						</v-container>
+					</v-card-text>
+				</v-card>
+			</v-flex>
+		</v-layout>
+	</v-container>
 </template>
 
 <script>
 	export default {
-		name: "index"
+		data () {
+			return {
+				email: '',
+				password: ''
+			}
+		},
+		computed: {
+			user () {
+			
+			},
+			error () {
+			
+			},
+			loading () {
+			
+			}
+		},
+		watch: {
+			user (value) {
+			
+			}
+		},
+		methods: {
+			onSignin () {
+				//this.$store.dispatch('signUserIn', {email: this.email, password: this.password})
+			},
+			onDismissed () {
+				//this.$store.dispatch('clearError')
+			}
+		}
 	}
 </script>
-
-<style scoped>
-
-</style>
